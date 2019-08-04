62280126177e663cb508072d9638e7a213113657
hieutranminh <hieu.tranminhhcm@gmail.com>
2019-08-04 22:45:48 +0700

    Done Product Detail

---
+++ assets/js/product.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  // var quantityProduct = $('.quantity')
+  var buttonIncrease = $('.btn-increase');
+  var buttonDecrease = $('.btn-decrease');
+  var quantityProduct;
+  // Increase quantity product
+  function increaseQuantity () {
+    buttonIncrease.on('click',function () {
+      // get input quantity with parent() & prev();
+      var inputQuantity = $(this).parent().prev();
+      // convert value string > value number
+      var quantityProduct = parseInt(inputQuantity.val());
+      quantityProduct++;
+      // convert value number > value string
+      quantityProduct = quantityProduct.toString();
+      inputQuantity.val(quantityProduct);
+    })
+  }
+  // Decrease quantity product
+  function decreaseQuantity () {
+    buttonDecrease.on('click',function () {
+      // get input quantity with parent() & prev();
+      var inputQuantity = $(this).parent().prev();
+      // convert value string > value number
+      var quantityProduct = parseInt(inputQuantity.val());
+      if(quantityProduct <= 1) {
+        quantityProduct = 1;
+      }else {
+        quantityProduct--;
+      }
+      // convert value number > value string
+      quantityProduct = quantityProduct.toString();
+      inputQuantity.val(quantityProduct);
+    })
+  }
+  increaseQuantity();
+  decreaseQuantity();
+})
